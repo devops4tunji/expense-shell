43fc49ee67f8a2a461ee1d998501156fa3b4bf7d
tunji <tunji.ijaola28@gmail.com>
2023-09-17 23:31:31 -0400

    chamges made to code

--- backend.sh
+++ backend.sh
@@ -21,4 +21,4 @@ systemctl start backend
 
 
 dnf install mysql -y
-mysql -h <MYSQL-SERVER-IPADDRESS> -uroot -pExpenseApp@1 < /app/schema/backend.sql
+mysql -h 172.31.22.40 -uroot -pExpenseApp@1 < /app/schema/backend.sql
