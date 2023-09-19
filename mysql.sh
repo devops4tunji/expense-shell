7d9ec0be2a6d8afb595f7cb388053b7e4f6cfba6
tunji <tunji.ijaola28@gmail.com>
2023-09-19 00:57:09 -0400

    correct syntax

--- mysql.sh
+++ mysql.sh
@@ -4,6 +4,7 @@ dnf module disable mysql -y >>/tmp/expense.log
 echo downloading the mysql community server
 dnf install mysql-community-server -y >>/tmp/expense.log
 
+
 echo copying mysql.repo into /yum.repos.d/mysql.repo
 cp mysql.repo /etc/yum.repos.d/mysql.repo >>/tmp/expense.log
 
@@ -14,5 +15,5 @@ systemctl start mysqld >>/tmp/expense.log
 echo setting mysql secure instalation
 mysql_secure_installation --set-root-pass ExpenseApp@1 >>/tmp/expense.log
 
-echo logging in to mysql
-mysql -uroot mysql.devopsafrican.online -pExpenseApp@1 < /app/schema/backend.sql >>/tmp/expense.log
+echo loading schema
+mysql -uroot  -pExpenseApp@1
