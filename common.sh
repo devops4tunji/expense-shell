bf8b76778e11811bcb99e86a6dbc82e03528e792
tunji <tunji.ijaola28@gmail.com>
2023-09-19 12:41:51 -0400

    using the &>> to redirect the outp and also adding -s to make the download silent

--- common.sh
+++ common.sh
@@ -6,8 +6,7 @@ log_file=/tmp/expense.log
 
 download_and_extract() {
 echo download $component code
-curl -s -o /tmp/$component.zip https://expense-artifacts.s3.amazonaws.com/$component.zip >>$log_file
-
+curl - s -o /tmp/$component.zip https://expense-artifacts.s3.amazonaws.com/$component.zip >>$log_file
 echo extracting frontend zip file
 unzip /tmp/$component.zip >>$log_file
 }
