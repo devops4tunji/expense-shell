39db97e28a1e77e39752574bd29474b7db3521c6
tunji <tunji.ijaola28@gmail.com>
2023-09-19 13:38:47 -0400

    adding echo line to see if my commands are working

--- backend.sh
+++ backend.sh
@@ -10,33 +10,47 @@ echo $? #this will let me know if it fails or does not fail.
 
 echo installing nodejs
 dnf install nodejs -y &>>$log_file
+echo $? #this will let me know if it fails or does not fail.
 
 echo copying backendservice to /system/backendservice
 cp $component2.service /etc/systemd/system/$component2.service &>>$log_file  #note: we brought this file upper because evertime i tried to run the command it was always telling me there was no file like that in that directory. so we had to bring it up because of  cd /app which came above it )
+echo $? #this will let me know if it fails or does not fail.
+
 
 echo removing /app directory
 rm -rf /app &>>$log_file # we added this because there was an error in the download process becasue we have cd/app in the script below
+echo $? #this will let me know if it fails or does not fail.
+
 
 echo adding user expense
 useradd expense &>>$log_file
+echo $? #this will let me know if it fails or does not fail.
 
 echo making a directory /app
 mkdir /app &>>$log_file
+echo $? #this will let me know if it fails or does not fail.
+
 echo changing changing directory
 cd /app &>>$log_file
+echo $? #this will let me know if it fails or does not fail.
 
 download_and_extract2
+echo $? #this will let me know if it fails or does not fail.
 
 echo installing node program manager
 npm install &>>$log_file
+echo $? #this will let me know if it fails or does not fail.
 
 echo systemctl reload
 systemctl daemon-reload &>>$log_file
 systemctl enable backend &>>$log_file
 systemctl start backend &>>$log_file
+echo $? #this will let me know if it fails or does not fail.
 
 echo installing mysql
 dnf install mysql -y &>>$log_file
+echo $? #this will let me know if it fails or does not fail.
 
 echo logging into mysql
 mysql -h mysql.devopsafrican.online -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>$log_file
+echo $? #this will let me know if it fails or does not fail.
--- frontend.sh
+++ frontend.sh
@@ -3,25 +3,32 @@ component=frontend #this means that im substituting the world frontend with comp
 #log_file=/tmp/expense.log
 #log_file=/tmp/expense.log will now be changed to source common.sh reference the common.sh file where all common coades are stored
 source common.sh #this was declared because i put log_file=/tmp/expense.log in the common.sh file
+echo $? #using this to see if the command failed or not
 
 echo installing nginx
 dnf install nginx -y &>>$log_file
+echo $? #using this to see if the command failed or not
 
 echo placing expense config file in nginx and redirecting it /tmp/expense.log
 cp expense.conf /etc/nginx/default.d/expense.conf &>>$log_file
+echo $? #using this to see if the command failed or not
 
 echo removing the /usr/share/nginx/html directory
 rm -rf /usr/share/nginx/html/* &>>$log_file
+echo $? #using this to see if the command failed or not
 
 echo changing directory to usr/share/nginx/html
 cd /usr/share/nginx/html &>>$log_file
+echo $? #using this to see if the command failed or not
 
 download_and_extract
-
+echo $? #using this to see if the command failed or not
 
 
 echo enabling nginx
 systemctl enable nginx &>>$log_file
+echo $? #using this to see if the command failed or not
 
 echo restarting nginx
 systemctl restart nginx &>>$log_file
+echo $? #using this to see if the command failed or not
